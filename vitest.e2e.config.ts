ec491b597806409fa2b37fa4d0c2f15e6b6388b0
Tal Wald <tjwald@gmail.com>
2025-12-24 19:37:10 +0200

    fix broken pnpm commands

---
+++ vitest.e2e.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vitest/config'
+import path from 'path'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@aspire/core': path.resolve(__dirname, './src/core'),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'node',
+    watch: false,
+    include: ['tests/**/e2e/**/*.test.{ts,tsx}'],
+  },
+})
